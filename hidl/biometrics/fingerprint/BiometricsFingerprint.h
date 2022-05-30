1f89484fd0f0f32121f0b315dd63d8b6380d72da
drkphnx <dark.phnx12@gmail.com>
2022-05-30 19:09:24 +0530

    remove override command as it gives issue with someroms
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- hidl/biometrics/fingerprint/BiometricsFingerprint.h
+++ hidl/biometrics/fingerprint/BiometricsFingerprint.h
@@ -78,8 +78,8 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
     bool mIsUdfps;
     UdfpsHandlerFactory* mUdfpsHandlerFactory;
     UdfpsHandler* mUdfpsHandler;
-    Return<void> onShowUdfpsOverlay() override;
-    Return<void> onHideUdfpsOverlay() override;
+    Return<void> onShowUdfpsOverlay();
+    Return<void> onHideUdfpsOverlay();
 };
 
 }  // namespace implementation
